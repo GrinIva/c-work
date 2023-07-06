1dc5425699f2691dd67817db9c95f71ff7cba7f0
GrinIv <svetili@bk.ru>
2023-07-07 01:46:30 +0300

    create get_summ project

---
+++ lesson1_summ/Program.cs
@@ -0,0 +1,9 @@
+﻿//int numberA = 3;
+//int numberB = 7;
+//Console.WriteLine(numberA + numberB);
+
+//second var
+int numberA = 7;
+int numberB = 7;
+int summ = numberA + numberB;
+Console.WriteLine(summ);
