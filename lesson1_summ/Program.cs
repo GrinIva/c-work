5cb47bd06e447599f800a047060f4f53e432715d
GrinIv <svetili@bk.ru>
2023-07-07 09:05:25 +0300

    get summ random

--- lesson1_summ/Program.cs
+++ lesson1_summ/Program.cs
@@ -3,7 +3,14 @@
 //Console.WriteLine(numberA + numberB);
 
 //second var
-int numberA = 7;
-int numberB = 7;
+//int numberA = 7;
+//int numberB = 7;
+//int summ = numberA + numberB;
+//Console.WriteLine(summ);
+
+int numberA = new Random().Next(1,10); // 1, 2, 3 ..  9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int summ = numberA + numberB;
 Console.WriteLine(summ);
