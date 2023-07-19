6e106dd2737a843572a0d96c3980be002d389502
GrinIv <svetili@bk.ru>
2023-07-19 21:06:24 +0300

    lection4 start picture

--- Lection4/Program.cs
+++ Lection4/Program.cs
@@ -14,13 +14,34 @@ for (int rows = 0; rows < 2; rows++)
 }
 */
 
-int[,] matrix = new int[3, 4];
-
-for (int i = 0; i < matrix.GetLength(0); i++)
+/*
+void PrintArray(int[,] matr)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        Console.Write($"{matrix[i,j]} ");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); // "[оператор Next воспринимает интервал в таком виде)"
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+*/
+
