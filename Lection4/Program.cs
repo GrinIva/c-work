a52fb3e74a0d4af20d07127008517003444bab56
GrinIv <svetili@bk.ru>
2023-07-19 19:36:40 +0300

    lection4 start

---
+++ Lection4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Двумерные массивы
+
+string[,] table = new string [2,5];
+table[1, 2] = "слово";
+
+
+for (int rows = 0; rows < length; rows++)
+{
+    for (int columns = 0; columns < length; columns++)
+    {
+        Consol.WriteLine ($"{table[row,columns]}")
+    }
+}
+
