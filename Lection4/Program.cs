73a4b8c39ba58829dede9f4475b36fdef5a33614
GrinIv <svetili@bk.ru>
2023-07-19 20:21:35 +0300

    lection4 GetLength

--- Lection4/Program.cs
+++ Lection4/Program.cs
@@ -16,9 +16,9 @@ for (int rows = 0; rows < 2; rows++)
 
 int[,] matrix = new int[3, 4];
 
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
         Console.Write($"{matrix[i,j]} ");
     }
