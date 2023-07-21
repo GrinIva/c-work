81dfe95822b43d1fb947fb46d182a7f0e8944e6f
GrinIv <svetili@bk.ru>
2023-07-22 01:01:33 +0300

    fobinachi end

--- Lection4/Program.cs
+++ Lection4/Program.cs
@@ -166,16 +166,19 @@ Console.WriteLine($"{i}! = {Factorial(i)}");
 
 //Рекурсия. Числа Фибоначи
 // f(1)=1, f(2)=1, f(n)=f(n-1)+f(n-2)
-
+/*
 int Fibonachi(int n)
 {
     if (n == 1 || n == 2) return 1;
     else return Fibonachi(n-1) + Fibonachi(n-2); 
 }
-for (int i = 1; i < 10; i++)
+for (int i = 1; i < 40; i++)
 {
     Console.WriteLine(Fibonachi(i));
 }
+
+*/
+
 
 //FillArray(matrix);
 //Console.WriteLine();
